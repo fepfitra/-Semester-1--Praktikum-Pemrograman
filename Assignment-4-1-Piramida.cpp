dbd0aa81da06ed37f375017f4b03d07b9918e43b
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-09-29 20:26:37 +0700

    just space

--- Assignment-4-1-Piramida.cpp
+++ Assignment-4-1-Piramida.cpp
@@ -9,11 +9,11 @@ int main(){
 
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n-i; j++){
-            cout << " ";
+            cout << "  ";
         }
 
         for (int j = 1; j <= i*2-1; j++){
-            cout << "*";
+            cout << "* ";
         }
 
         cout << endl;
