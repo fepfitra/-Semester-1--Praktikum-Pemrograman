6a56ed53d2da788b71ca926fdb44d131d0bd6916
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-10-12 22:11:27 +0700

    fix error intersect array

--- Homework-5-2-intersect-array.cpp
+++ Homework-5-2-intersect-array.cpp
@@ -27,7 +27,7 @@ int main(){
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(a[i] == a[j]){
+            if(a[i] == b[j]){
                 cout << a[i] << endl;
             }
         }
